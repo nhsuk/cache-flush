6df7ddd5aea86494e2441b7e76984e11e11b9b2d
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-16 15:21:36 +0100

    :eyes: Include response when not successful

--- scripts/test-app-is-running.sh
+++ scripts/test-app-is-running.sh
@@ -10,7 +10,10 @@ APP_SERVICE_URL=$APP_SERVICE_HOST/api/FastPurgeUrls
 # Use the env var set from the previous task with the path to the function app
 echo "Going to make request to function app: '$APP_SERVICE_URL'. "
 
-HTTP_STATUS=$(curl -sS -XPOST "$APP_SERVICE_URL" -H "Content-Type: application/json" -d "@./scripts/test-payload.json" | jq '.httpStatus == 400')
+RES_OUT=func-app-response.json
+
+curl -sS -o "$RES_OUT" -XPOST "$APP_SERVICE_URL" -H "Content-Type: application/json" -d "@./scripts/test-payload.json"
+HTTP_STATUS=$(jq '.httpStatus' $RES_OUT)
 echo "Got status '$HTTP_STATUS' from the response body of the request made to the function app."
 
 # httpStatus (HTTP_STATUS) should be 400. This is the status code from the
@@ -19,5 +22,7 @@ echo "Got status '$HTTP_STATUS' from the response body of the request made to th
 # rejected as a bad request.
 if [ "$HTTP_STATUS" != "400" ]; then
   echo "HTTP Status code was not 400. Please confirm the function app has been deployed successfully."
+  echo "The response was:"
+  jq '' $RES_OUT
   exit 1
 fi
