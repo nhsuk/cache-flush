99ac8c96f45a4fd51b0a8a47cee521c6e37f4e06
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-16 16:45:32 +0100

    :eyes: Improve output message

--- scripts/test-app-is-deployed.sh
+++ scripts/test-app-is-deployed.sh
@@ -9,11 +9,12 @@ fi
 echo "Going to make request to function app: '$APP_SERVICE_HOST'. "
 
 HTTP_STATUS=$(curl -sS -o /dev/null -w "%{http_code}" -XGET "$APP_SERVICE_HOST")
-echo "Got status '$HTTP_STATUS'."
 
 # This is a basic test to check the function app has been deployed. If the
 # deployment has failed no site exists.
-if [ "$HTTP_STATUS" != "200" ]; then
+if [ "$HTTP_STATUS" = "200" ]; then
+  echo "Got status '$HTTP_STATUS'. The app has been deployed."
+else
   echo "HTTP Status code was not 200. It looks like the app has not been deployed successfully."
   exit 1
 fi
