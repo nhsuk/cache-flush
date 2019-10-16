db70afff40742f6eb80ceb9800cbe9c7bdde7957
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-16 12:18:21 +0100

    :twisted_rightwards_arrows: Use app URL for test

--- scripts/test-app-is-running.sh
+++ scripts/test-app-is-running.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Use the env var set from the previous task with the path to the function app
-echo "Going to make request to function app: '$FUNC_APP_APPSERVICEAPPLICATIONURL'. "
+echo "Going to make request to function app: '$APP_SERVICE_URL'. "
 
-HTTP_STATUS="$(curl -s -o /dev/null -w '%{http_code}' -XGET "$FUNC_APP_APPSERVICEAPPLICATIONURL")"
+HTTP_STATUS="$(curl -s -o /dev/null -w '%{http_code}' -XGET "$APP_SERVICE_URL")"
 echo "Got status: '$HTTP_STATUS'."
 
 if [ "$HTTP_STATUS" != "200" ]; then
