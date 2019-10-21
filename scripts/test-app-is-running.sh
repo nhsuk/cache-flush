0eb86d40a5db76bd82457d89d5f469e015fa1189
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-21 09:31:08 +0100

    :eyes: Improve response variable naming

--- scripts/test-app-is-running.sh
+++ scripts/test-app-is-running.sh
@@ -10,10 +10,10 @@ APP_SERVICE_URL=$APP_SERVICE_HOST/api/FastPurgeUrls
 # Use the env var set from the previous task with the path to the function app
 echo "Going to make request to function app: '$APP_SERVICE_URL'. "
 
-RES_OUT=func-app-response.json
+RESPONSE=func-app-response.json
 
-curl -sS -o "$RES_OUT" -XPOST "$APP_SERVICE_URL?code=$FUNCTION_KEY" -H "Content-Type: application/json" -d "@./scripts/resources/test-payload.json"
-HTTP_STATUS=$(jq '.httpStatus' $RES_OUT)
+curl -sS -o "$RESPONSE" -XPOST "$APP_SERVICE_URL?code=$FUNCTION_KEY" -H "Content-Type: application/json" -d "@./scripts/resources/test-payload.json"
+HTTP_STATUS=$(jq '.httpStatus' $RESPONSE)
 echo "Got status '$HTTP_STATUS' from the response body of the request made to the function app."
 
 # httpStatus (HTTP_STATUS) should be 400. This is the status code from the
@@ -23,6 +23,6 @@ echo "Got status '$HTTP_STATUS' from the response body of the request made to th
 if [ "$HTTP_STATUS" != "400" ]; then
   echo "HTTP Status code was not 400. Please confirm the function app has been deployed successfully."
   echo "The response was:"
-  jq '' $RES_OUT
+  jq '' $RESPONSE
   exit 1
 fi
