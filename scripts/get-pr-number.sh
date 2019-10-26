eda4b7421db49582950a772c88ecc1cb4c495cc9
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-26 22:27:23 +0100

    NOTHING

---
+++ scripts/get-pr-number.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+regex="#([0-9]+)"
+echo "Testing commit msg: '$1' against regex: '$regex'."
+echo "$1"
+
+if [[ "$1" =~ $regex ]]; then
+  echo "PR number found. Value is: ${BASH_REMATCH[1]}"
+  echo "##vso[task.setvariable variable=PR_NUMBER;isOutput=true]${BASH_REMATCH[1]}"
+else
+  echo "No PR number found"
+fi;
