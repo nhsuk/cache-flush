b98847ef4536aee1de125cccacacbdf472cab5a6
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-10-27 16:45:34 +0000

    TEST

--- scripts/get-pr-number.sh
+++ scripts/get-pr-number.sh
@@ -3,8 +3,6 @@
 regex="#([0-9]+)"
 echo "Testing commit msg: '$1' against regex: '$regex'."
 
-echo "$1"
-
 if [[ "$1" =~ $regex ]]; then
   echo "PR number found. Value is: ${BASH_REMATCH[1]}"
   echo "##vso[task.setvariable variable=PR_NUMBER;isOutput=true]${BASH_REMATCH[1]}"
